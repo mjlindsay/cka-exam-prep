62f2d93a7938a1b4e74b5ac22c5ab4bf2f1d2d8e
Michael Lindsay <michael.lindsay.j@gmail.com>
2025-05-04 13:39:23 -0400

    feat: switch back to pxelinux and use preseed injection into initrd.gz

--- dns-dhcp/entrypoint.sh
+++ dns-dhcp/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NETBOOT_ARCHIVE="${NETBOOT_ARCHIVE:-/etc/netboot.tar.gz}"
+PRESEED_FILE="${PRESEED_FILE:-/etc/preseed.cfg}"
 
 if [ -f $NETBOOT_ARCHIVE ]; then
     echo "Found archive at $NETBOOT_ARCHIVE"
@@ -18,5 +19,22 @@ cp /tftpboot/debian-installer/amd64/grubx64.efi /tftpboot/grubx64.efi
 touch /tftpboot/revocations.efi
 
 tar -C $TFTPBOOT_BASE_DIR -xzf $NETBOOT_ARCHIVE
+
+if [ -f $PRESEED_FILE ]; then
+    echo "Found preseed file"
+
+    if [-f /tftpboot/debian-installer/initrd.gz ]; then
+        echo "Found initrd.gz, injecting preseed file"
+
+        gunzip /tftpboot/debian-installer/amd64/initrd.gz
+        chmod +w /tftpboot/debian-installer/initrd
+        echo $PRESEED_FILE | cpio -H newc -o -A -F /tftpboot/debian-installer/amd64/initrd.gz
+        gzip /tftpboot/debian-installer/amd64/initrd
+    else
+        echo "Could not find initrd.gz to inject preseed file"
+    fi
+else
+    echo "Skipping preseed injection as file not found at $PRESEED_FILE"
+fi
 
 dnsmasq --no-daemon
