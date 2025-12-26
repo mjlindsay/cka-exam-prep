5528c1082c516be627ec25207e1cab0f93e85fb1
mjlindsay <michael.lindsay.j@gmail.com>
2025-12-26 15:21:15 -0500

    setup cert creation and distribution for cluster nodes

---
+++ kubernetes-the-hard-way/certs/dist-certs.sh
@@ -0,0 +1,15 @@
+# Copy the certs to the worker machines
+hosts=("kdev2" "kdev3")
+
+for host in $hosts; do
+    ssh root@${host} mkdir /var/lib/kubelet
+
+    scp ca.crt root@${host}:/var/lib/kubelet
+    scp ${host}.crt root@${host}:/var/lib/kubelet/kubelet.crt
+    scp ${host}.key root@${host}:/var/lib/kubelet/kubelet.key
+done
+
+
+# Copy certs to the control machine
+scp ca.key ca.crt kube-api-server.key kube-api-server.crt \
+    service-accounts.key service-accounts.crt root@${kdev1}
---
+++ kubernetes-the-hard-way/certs/gen-certs.sh
@@ -0,0 +1,31 @@
+# Generate the CA
+openssl genrsa -out "ca.key" 4096
+openssl req -x509 -new -sha512 -noenc \
+    -key ca.key -days 3653 \
+    -config ca-custom.conf \
+    -out ca.crt
+
+# Generate Client/Server certs for components
+certs=(
+  "admin" "kdev2" "kdev3"
+  "kube-proxy" "kube-scheduler"
+  "kube-controller-manager"
+  "kube-api-server"
+  "service-accounts"
+)
+
+for i in ${certs[*]}; do
+  openssl genrsa -out "${i}.key" 4096
+
+  openssl req -new -key "${i}.key" -sha256 \
+    -config "ca-custom.conf" -section ${i} \
+    -out "${i}.csr"
+
+  openssl x509 -req -days 3653 -in "${i}.csr" \
+    -copy_extensions copyall \
+    -sha256 -CA "ca.crt" \
+    -CAkey "ca.key" \
+    -CAcreateserial \
+    -out "${i}.crt"
+done
+
