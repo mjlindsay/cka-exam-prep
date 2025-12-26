788dc64fb9f739e852094f4c057e93712ca7e7c3
mjlindsay <michael.lindsay.j@gmail.com>
2025-12-26 15:30:46 -0500

    split generation and distribution of kubeconfigs

---
+++ kubernetes-the-hard-way/configs/dist-kubeconfigs.sh
@@ -0,0 +1,14 @@
+hosts=("kdev2" "kdev3")
+
+for host in $hosts; do
+	ssh root@${host} "mkdir -p /var/lib/{kube-proxy,kubelet}"
+	
+	scp kube-proxy.kubeconfig root@${host}:/var/lib/kube-proxy/kubeconfig
+
+	scp ${host}.kubeconfig root@${host}:/var/lib/kubelet/kubeconfig
+done
+
+scp admin.kubeconfig \
+  kube-controller-manager.kubeconfig \
+  kube-scheduler.kubeconfig \
+  root@kdev1:~/
--- kubernetes-the-hard-way/configs/gen-kubeconfigs.sh
+++ kubernetes-the-hard-way/configs/gen-kubeconfigs.sh
@@ -106,16 +106,3 @@ kubectl config set-context default \
 
 kubectl config use-context default \
 	--kubeconfig=admin.kubeconfig
-
-for host in $hosts; do
-	ssh root@${host} "mkdir -p /var/lib/{kube-proxy,kubelet}"
-	
-	scp kube-proxy.kubeconfig root@${host}:/var/lib/kube-proxy/kubeconfig
-
-	scp ${host}.kubeconfig root@${host}:/var/lib/kubelet/kubeconfig
-done
-
-scp admin.kubeconfig \
-  kube-controller-manager.kubeconfig \
-  kube-scheduler.kubeconfig \
-  root@kdev1:~/
